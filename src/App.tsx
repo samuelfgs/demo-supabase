c37acbd602f1ef9d0c221ad288b09517fdb92409
Samuel <samuel@plasmic.app>
2022-02-09 16:56:39 -0300

    supabase demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Homepage from "./components/Homepage";
+import { PlasmicCanvasHost } from '@plasmicapp/loader-react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { PLASMIC } from './plasmic-init';
+import LoginPage from "./components/LoginPage";
+import AddPost from "./components/AddPost";
+import Mountain from "./components/Mountain";
+import PostUpdate from "./components/PostUpdate";
+import SignupPage from "./components/SignupPage";
 
-function App() {
+function Index() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    PLASMIC &&
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} /> 
+        <Route path="/login-page" element={<LoginPage />} />
+        <Route path="/signup-page" element={<SignupPage />} />
+        <Route path="/post/add" element={<AddPost />} /> 
+        <Route path="/post/update/id" element={<PostUpdate />} /> 
+        <Route path="/mountain" element={<Mountain />} />
+        <Route path="/plasmic-host" element={<PlasmicCanvasHost />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+export default Index;
